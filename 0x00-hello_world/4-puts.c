449a4982b86f64ba888ec85838ddad66d91ab856
Ugonna Erondu <kelvinpiano@gmail.com>
2023-02-16 19:30:31 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that outputs a line of text with the puts function'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
