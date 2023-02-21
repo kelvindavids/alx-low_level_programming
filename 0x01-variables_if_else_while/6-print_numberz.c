816057d5fe973da6406a23683e5d5fd0acd30f92
Ugonna Erondu <kelvinpiano@gmail.com>
2023-02-20 16:24:11 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Starting point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+for (x = 0; x < 9; x++)
+putchar(x);
+putchar('\n');
+
+return (0);
+}
