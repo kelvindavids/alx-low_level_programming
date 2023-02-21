ce9c8af45b8eb5f74083c36b05a19a043774108f
Ugonna Erondu <kelvinpiano@gmail.com>
2023-02-20 17:59:25 -0800

    102-print-comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@
 int main(void)
 {
 int y, z;
-for (y = '0'; z < 98; y++)
+for (y = 0; z < 98; y++)
 {
 for (z = y + 1; z <= 99; z++)
 {
-putchar((y / 10) + 0);
-putchar((y % 10) + 0);
+putchar((y / 10) + '0');
+putchar((y % 10) + '0');
 putchar(' ');
-putchar((z / 10) + 0);
-putchar((z % 10) + 0);
+putchar((z / 10) + '0');
+putchar((z % 10) + '0');
 if (y == 98 && z == 99)
 continue;
 putchar(',');
