323811628926df271736bda196ada9d855039999
Ugonna Erondu <kelvinpiano@gmail.com>
2023-02-22 14:35:06 -0800

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add to numbers
+ * @n1: the 1st num to be computed
+ * @n2: the 2nd num to be  computed
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+
+}
